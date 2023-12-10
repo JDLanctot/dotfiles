9fdd9e560ad7141c02792c866ea1b42be75cf0bf
JDLanctot <jordan.lanctot@ryerson.ca>
2023-12-10 15:57:58 -0500

    Revert "Update packer.lua"
    This reverts commit 4fde13905f9ed38dfa6cd24ca45284caacc7c0d4.

--- .config/nvim/lua/toofaeded/packer.lua
+++ .config/nvim/lua/toofaeded/packer.lua
@@ -31,13 +31,7 @@ return require('packer').startup(function(use)
           }
       end
   })
-		
- use({
-      "nvim-tree/nvim-tree.lua",
-      requires ={
-	  'nvim-tree/nvim-web-devicons',
-      },
- })
+
 
   use({"nvim-treesitter/nvim-treesitter", run = ":TSUpdate"})
   use("nvim-treesitter/playground")
