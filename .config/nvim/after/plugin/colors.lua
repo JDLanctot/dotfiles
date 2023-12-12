d7a95fed2cc3950d0e91f375a3c73a1296c16b61
JDLanctot <jordan.lanctot@ryerson.ca>
2023-12-12 10:54:51 -0500

    Better indications of errors

--- .config/nvim/after/plugin/colors.lua
+++ .config/nvim/after/plugin/colors.lua
@@ -2,9 +2,7 @@ require('rose-pine').setup({
     disable_background = true
 })
 
-vim.g.everforest_diagnostic_line_highlight = 1
-
-function ColorMyPencils(color) 
+function ColorMyPencils(color)
 	color = color or "rose-pine"
 	vim.cmd.colorscheme(color)
 
