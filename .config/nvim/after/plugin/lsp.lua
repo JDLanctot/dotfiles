d8a6f48d62c18353bfa1ad5c64201d57e010d883
Jordan D. Lanctot <45880087+JDLanctot@users.noreply.github.com>
2023-12-12 10:35:04 -0500

    Update lsp.lua

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -59,3 +59,31 @@ lsp.setup()
 vim.diagnostic.config({
     virtual_text = true
 })
+
+-- error lens
+vim.fn.sign_define {
+  {
+    name = 'DiagnosticSignError',
+    text = '',
+    texthl = 'DiagnosticSignError',
+    linehl = 'ErrorLine',
+  },
+  {
+    name = 'DiagnosticSignWarn',
+    text = '',
+    texthl = 'DiagnosticSignWarn',
+    linehl = 'WarningLine',
+  },
+  {
+    name = 'DiagnosticSignInfo',
+    text = '',
+    texthl = 'DiagnosticSignInfo',
+    linehl = 'InfoLine',
+  },
+  {
+    name = 'DiagnosticSignHint',
+    text = '',
+    texthl = 'DiagnosticSignHint',
+    linehl = 'HintLine',
+  },
+}
