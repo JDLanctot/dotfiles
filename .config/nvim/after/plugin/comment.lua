9080be9f9359dacbc5972bf8a1a6513153a28a77
JDLanctot <jordan.lanctot@ryerson.ca>
2023-12-12 19:34:49 -0500

    'Force the keymaps for comment'

--- .config/nvim/after/plugin/comment.lua
+++ .config/nvim/after/plugin/comment.lua
@@ -1,4 +1,27 @@
-require("Comment").setup()
+require("Comment").setup({
+    padding = true,
+    sticky = true,
+    ignore = nil,
+    toggler = {
+        line = 'gcc',
+        block = 'gbc',
+    },
+    opleader = {
+        line = 'gc',
+        block = 'gb',
+    },
+    extra = {
+        above = 'gcO',
+        below = 'gco',
+        eol = 'gcA',
+    },
+    mappings = {
+        basic = true,
+        extra = true,
+    },
+    pre_hook = nil,
+    post_hook = nil,
+})
 
 vim.keymap.set('n', ']z', function()
     require('todo-comments').jump_next()
