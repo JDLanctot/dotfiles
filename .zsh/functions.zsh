a931ffb497f98e8217fe63f6e7772c3aeb94870b
Jordan D. Lanctot <45880087+JDLanctot@users.noreply.github.com>
2024-08-28 15:57:20 -0400

    Update functions.zsh

--- .zsh/functions.zsh
+++ .zsh/functions.zsh
@@ -2,3 +2,8 @@
 function colormap() {
   for i in {0..255}; do print -Pn "%K{$i}  %k%F{$i}${(l:3::0:)i}%f " ${${(M)$((i%6)):#3}:+$'\n'}; done
 }
+
+# Conda Shorthand
+function ca() {
+    conda activate "$1"
+}
